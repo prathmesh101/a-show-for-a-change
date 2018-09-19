6064d7aa11d9a887a58d323d115d2403ab4ae324
Alia Wilkinson <aliawilkinson@gmail.com>
2018-09-19 15:49:32 -0700

    header/footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer-cont">
+            Footer
+        </div>
+    )
+}
+
+export default Footer;
