723f6954f9762abe292eb00942a779e33ca33584
Alex Choi <alexchoiweb@gmail.com>
2018-11-06 04:37:18 +0000

    test dev branch commit

--- src/components/nav-pages/sign-up.js
+++ src/components/nav-pages/sign-up.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-const SignUp = () => {
-    return (
-        <div>sign up</div>
-    )
+class SignUp extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			fullName: null
+		}
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(event.target.value);
+
+		const user = {
+				name: this.state.fullName
+		}
+
+		axios.post(`https://jsonplaceholder.typicode.com/users`, { user: user })
+			.then(res => {
+				console.log(res);
+				console.log(data);
+			})
+	}
+
+	handleInputChange = (event) => {
+		event.preventDefault();
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	render () {
+		const {fullName} = this.state;
+		return (
+			<div>
+				<h1>Sign Up Form</h1>
+				<p>Full Name is {fullName}</p>
+				<form onSubmit={this.handleSubmit}>
+					<p><input type='text' name='fullName' placeholder='Your Name' onChange={this.handleInputChange}></input></p>
+					<p><button>Register</button></p>
+				</form>
+			</div>
+		)
+	}
 }
 
 export default SignUp;
