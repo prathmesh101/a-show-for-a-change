f19013bd20c50d317da833533f5b31bf13cedbdc
Alex Choi <alexchoiweb@gmail.com>
2018-11-19 23:54:43 +0000

    update home page for mobile

--- src/components/nav-pages/home.js
+++ src/components/nav-pages/home.js
@@ -6,9 +6,35 @@ import Video from '../video';
 const Home = () => {
 	return (
 		<div className="wrapper">
-			<div className="one">
-				<p id="headerText">See with your eyes, then tip with your heart.</p>
-				<p id="headerParagraph">Watch a curated set of socially responsible movies, starting at $4.99/month. See content that will not only move you, but allow you to support independent filmmakers, celebrity influencers and their connected causes. <a href="/about">Want to learn more?</a></p>
+      <div className="div-video">
+        <video controls>
+          <source src='http://dl1cz9lqgee4z.cloudfront.net/Cumplaneara.mp4' type='video/mp4' />
+        </video>			
+      </div>
+			<div className='div-header'>
+				<div className='div-underVideo'>
+				<p>Watch 3 For Free</p>
+				<span><a href="/" className="linkPrimary">NEXT</a></span>
+				</div>
+				<div className="one">
+				<p id="headerText">Watch. Learn. Give.</p>
+				<p id="headerParagraph">Sage Age offers a growing array of socially and environmentally responsible short films, and allows you to donate to the causes closest to your heart.</p>
+				<div className="div-buttonWatchFree">
+					<a href="sign-up"><button id="buttonWatchFree">WATCH FREE FOR 30 DAYS</button></a>
+				</div>
+				<div className="div-disclaimer">
+					<p>*Only $4.99/month or $49.99/year</p>
+				</div>
+				<div className="div-infoLinks">
+					<a href="/" className="linkPrimary">How It Works</a>
+					<span><a href="/about" className="linkPrimary">About Us</a></span>
+				</div>
+			</div>
+
+
+
+
+
 				<div className="div-images">
 					<img src="https://m.media-amazon.com/images/M/MV5BMjM3MzE2OTEwNF5BMl5BanBnXkFtZTcwNTkyMTg4NA@@._V1_UX182_CR0,0,182,268_AL_.jpg"></img>
 					<img src="https://m.media-amazon.com/images/M/MV5BMjA3NjkzNjg2MF5BMl5BanBnXkFtZTgwMDkyMzgzMDI@._V1_UY268_CR0,0,182,268_AL_.jpg"></img>
@@ -30,6 +56,7 @@ const Home = () => {
 			<div className="three">
 				<div className="div-headerBar">
 					<div className="div-watchTrailer">
+						{/* <img src="../../assets/img/icon-imgs/video-camera.svg"></img> */}
 						<p>Watch Trailer</p>
 					</div>
 					<div className="div-supportCreators">
@@ -39,6 +66,9 @@ const Home = () => {
 						<p>Browse Videos</p>
 					</div>
 				</div>
+			</div>
+			<div className="four">
+				<div className="div-trailerBar"></div>
 			</div>
 		</div>
 	)
