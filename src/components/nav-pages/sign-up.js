4d8ee8aba22297652d914d2c0c13266107abc8fb
Sivak Khodaverdian <sevak7@yahoo.com>
2018-11-28 10:46:43 -0800

    fixing api call issue

--- src/components/nav-pages/sign-up.js
+++ src/components/nav-pages/sign-up.js
@@ -26,7 +26,7 @@ class SignUp extends Component {
 
 
   handleClick(event) {
-    var apiBaseUrl = "../API_PHP/signupapi.php";
+    var apiBaseUrl = "../../../API_PHP/signupapi.php";
     console.log("values", this.state.first_name, this.state.last_name, this.state.email, this.state.password);
     alert("values are " + this.state.first_name + "   " + this.state.email);
     //To be done:check name empty values before hitting submit
