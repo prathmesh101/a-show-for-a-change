c8d85454aec6c7a54b577b2ed2bed6047b3edfe0
Sivak Khodaverdian <sevak7@yahoo.com>
2018-11-30 00:03:36 -0800

    fixing api call issue

--- src/backend/php/init.php
+++ src/backend/php/init.php
@@ -3,25 +3,20 @@ $app = __DIR__;
 
 //require_once('/etc/php/7.0/cli/vendor/autoload.php');
 
-
-file_put_contents("../../../backup/phpfilereached.txt",  $app   ."\n");
 require_once("{$app}/config.php");
-echo "afte config.php";
-exit;
 //require_once("{$app}/includes/classes/Stripe_API.php");
-require_once("{$app}/src/php/Session.php");
-require_once("{$app}/src/php/Database.php");
-require_once("{$app}/src/php/User.php");
+require_once("{$app}/Session.php");
+require_once("{$app}/Database.php");
+require_once("{$app}/Users.php");
 /*require_once("{$app}/includes/classes/Dealer.php");
 require_once("{$app}/includes/classes/Plan.php");
 require_once("{$app}/includes/classes/Subscription.php");
 require_once("{$app}/includes/classes/Calculate.php");
-*/
+
 $public = strpos($_SERVER['SCRIPT_NAME'], '/html') + 5;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, 0);
 define("WWW_ROOT", $doc_root);
 
 //require_once("{$app}/includes/functions/functions.php");
 $errors=[];
-
-?>
+*/
