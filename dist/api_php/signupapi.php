b86ff7c6bbb4f9e9f4fa46ec8ca0e1adb0982d32
Sivak Khodaverdian <sevak7@yahoo.com>
2018-12-03 15:30:40 -0800

    api calls

--- dist/api_php/signupapi.php
+++ dist/api_php/signupapi.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: REQUEST");
-header("Access-Control-Max-Age: 3600");
+//header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require_once("../../src/backend/php/init.php");
