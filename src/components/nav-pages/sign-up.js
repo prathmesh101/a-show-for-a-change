e1c0217a59fc4f3beb188d688c24c53375d0d3c9
Sivak Khodaverdian <sevak7@yahoo.com>
2018-12-04 17:06:35 -0800

    api calls

--- src/components/nav-pages/sign-up.js
+++ src/components/nav-pages/sign-up.js
@@ -31,14 +31,17 @@ class SignUp extends Component {
     alert("values are " + this.state.first_name + "   " + this.state.email);
     //To be done:check name empty values before hitting submit
     // var self = this;
-    var payload = {
-      "first_name": this.state.first_name,
-      "last_name": this.state.last_name,
-      "email": this.state.email,
-      "password": this.state.password
-    };
+    // var payload = {
+    //   "first_name": this.state.first_name,
+    //   "last_name": this.state.last_name,
+    //   "email": this.state.email,
+    //   "password": this.state.password,
+    // };
     //var payload = first_name: this.state.first_name;
-    axios.post(apiBaseUrl, payload)
+    const formData = new FormData();
+    formData.append('key', value);
+
+    axios.post(apiBaseUrl, formData)
       .then(function (response) {
         alert("here");
         alert(JSON.stringify(response));
