94aeb0b6a46f03a004e858cd7d9331675328ff31
Sivak Khodaverdian <sevak7@yahoo.com>
2018-12-06 15:57:40 -0800

    removing sensitive data

--- src/backend/php/config.php
+++ src/backend/php/config.php
@@ -1,6 +1,6 @@
 <?php
-defined("DB_SERVER") ? null : define("DB_SERVER", "localhost");
-defined("DB_USER") ? null : define("DB_USER", "root");
-defined("DB_PASS") ? null : define("DB_PASS", "root");
-defined("DB_NAME") ? null : define("DB_NAME", "db_ashowforachange");
+defined("DB_SERVER") ? null : define("DB_SERVER", "ashowforachange.cisky1csna1s.us-west-2.rds.amazonaws.com");
+defined("DB_USER") ? null : define("DB_USER", "causfx");
+defined("DB_PASS") ? null : define("DB_PASS", "rootRoot2");
+defined("DB_NAME") ? null : define("DB_NAME", "ashowforachange");
 ?>
