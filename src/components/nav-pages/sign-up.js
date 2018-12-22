8aab64a68deb1ebcf467d5b961f6e54df224facd
prathmesh101 <prathmesh.pardhiye@gmail.com>
2018-12-21 16:02:21 -0800

    sign up navigate to user-page

--- src/components/nav-pages/sign-up.js
+++ src/components/nav-pages/sign-up.js
@@ -1,68 +1,74 @@
 import React, { Component } from 'react';
-import 'bootstrap';
-import axios from 'axios';
+ import 'bootstrap';
+ import axios from 'axios';
 import '../../assets/css/sign-up.css';
 import { Link } from 'react-router-dom';
+import UserPage from './user-page.js';
 import { Redirect } from 'react-router';
-
 
 class SignUp extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
+     super(props);
+     this.state = {
       first_name: '',
       last_name: '',
       email: '',
       password: '',
       redirect: false
-
     }
   }
 
 
-  handleChange = this.handleChange.bind(this);
-  handleClick = this.handleClick.bind(this);
+   handleChange = this.handleChange.bind(this);
+   handleClick = this.handleClick.bind(this);
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+   handleChange(event) {
+     this.setState({ [event.target.name]: event.target.value })
+   }
 
 
-  async handleClick(event) {
 
-    var apiBaseUrl = "https://dev.sageape.com/api_php/signupapi.php";
-
-    //To be done:check name empty values before hitting submit
-    var self = this;
-    var params = {
-      "first_name": this.state.first_name,
-      "last_name": this.state.last_name,
-      "email": this.state.email,
-      "password": this.state.password,
+   handleClick(event) {
+     event.preventDefault();
+     var apiBaseUrl = "https://dev.sageape.com/api_php/signupapi.php";
+     console.log("values", this.state.first_name, this.state.last_name, this.state.email, this.state.password);
+     //To be done:check name empty values before hitting submit
+     var self = this;
+     var params = {
+       "first_name": this.state.first_name,
+       "last_name": this.state.last_name,
+       "email": this.state.email,
+       "password": this.state.password,
     };
     //var payload = this.state.first_name;
+    axios.post(apiBaseUrl, {first_name: this.state.first_name, last_name: this.state.last_name, email: this.state.email, password: this.state.password})
+        .then(() => this.setState({ redirect: true }));
 
-     axios.post(apiBaseUrl, { first_name: this.state.first_name, last_name: this.state.last_name, email: this.state.email, password: this.state.password })
+  
 
 
+
+/*
       .then(function (response) {
-        alert("here");
-        alert(JSON.stringify(response));
-        console.log(response);
+        //this.props.history.push('/UserPage');
+        // alert("here");
+        // alert(JSON.stringify(response));
+        // console.log(response);
         // console.log(response);
         //var res = JSON.parse(response);
         if (response.status == 200) {
-        //if (true) {
+      //  if (response.status == 200) {
           //if (response == "success") {
           console.log("registration successfull");
           alert("status is 200");
-          this.setState({ redirect: true })
+      //    console.log("registration successfull");
+        //  alert("status is 200");
           //var loginscreen = [];
           // loginscreen.push(<Login parentContext={this} />);
           //var loginmessage = "Not Registered yet.Go to registration";
-          // self.props.parentContext.setState({
-          //   loginscreen: loginscreen,
-          //   loginmessage: loginmessage,
+           // self.props.parentContext.setState({
+           //   loginscreen: loginscreen,
+           //   loginmessage: loginmessage,
           //   buttonLabel: "Register",
           //   isLogin: true
           // });
@@ -74,8 +80,15 @@ class SignUp extends Component {
         console.log(error);
         alert("Error is " + error.response + "  " + error.response.status + "  " + JSON.stringify(error.response.headers));
       });
-
-
+      //  } else {
+        //  alert("not equal to 200 but what it is we don't know");
+      //  }
+    //  })
+  //    .catch(function (error) {
+    //    console.log(error);
+      //  alert("Error is " + error.response + "  " + error.response.status + "  " + JSON.stringify(error.response.headers));
+    //  });
+*/
   }
 
 
@@ -86,6 +99,7 @@ class SignUp extends Component {
     if (redirect) {
          return <Redirect to='./user-page'/>;
        }
+
 
     return (
 
