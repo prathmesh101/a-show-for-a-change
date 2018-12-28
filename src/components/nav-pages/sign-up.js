3e01b0108849c62ee8f07ee5ed1d60e32b3dbb14
Alex Choi <alexchoiweb@gmail.com>
2018-12-28 02:31:06 +0000

    style signin page

--- src/components/nav-pages/sign-up.js
+++ src/components/nav-pages/sign-up.js
@@ -100,34 +100,33 @@ class SignUp extends Component {
          return <Redirect to='./user-page'/>;
        }
 
-
     return (
 
-      <div>
+      <div className="div-signUp">
         <form>
           <div className="form-group">
-            <h2 className="signup-head-text">Create Your Account</h2>
+            <h2 className="headText">Create Your Account</h2>
             <div className="div-underline">
               <i className="fas fa-user inlineBlock fillWhite"></i>
-              <input type="text" className="form-control inlineBlock" name="first_name" id="firstname" placeholder="Enter first name" size="10" onChange={this.handleChange} />
+              <input type="text" className="inlineBlock" name="first_name" id="firstname" placeholder="Enter first name" size="10" onChange={this.handleChange} />
             </div>
           </div>
           <div className="form-group">
             <div className="div-underline">
               <i className="fas fa-user inlineBlock fillWhite"></i>
-              <input type="text" className="form-control inlineBlock" name="last_name" id="lastname" placeholder="Enter last name" onChange={this.handleChange} />
+              <input type="text" className="inlineBlock" name="last_name" id="lastname" placeholder="Enter last name" onChange={this.handleChange} />
             </div>
           </div>
           <div className="form-group">
             <div className="div-underline">
               <i className="fas fa-envelope fillWhite"></i>
-              <input type="email" className="form-control inlineBlock" name="email" id="email" placeholder="Enter email" onChange={this.handleChange} />
+              <input type="email" className="inlineBlock" name="email" id="email" placeholder="Enter email" onChange={this.handleChange} />
             </div>
           </div>
           <div className="form-group">
             <div className="div-underline">
               <i className="fas fa-unlock-alt fillWhite"></i>
-              <input type="password" className="form-control inlineBlock" name="password" id="password" placeholder="Enter password" onChange={this.handleChange} />
+              <input type="password" className="inlineBlock" name="password" id="password" placeholder="Enter password" onChange={this.handleChange} />
             </div>
           </div>
           <div className="div-submit">
