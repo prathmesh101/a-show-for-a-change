2eb1fa75cfb3e060a57fcd33a3f447071e278367
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-08 15:38:49 -0800

    fixing main menu navigation issues

--- src/components/nav-pages/navbar.js
+++ src/components/nav-pages/navbar.js
@@ -1,18 +1,33 @@
 import React from 'react';
 //import 'bootstrap';
-import Home from './home.js';
+import Partners from './partners.js';
 import About from './about.js';
+import Causes from './partners.js';
+import GENRE from './genre.js';
+import CHANNELS from './partners.js';
 import '../../assets/css/navbar.css';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
       <nav>
-        <NavLink to="/">
+        <NavLink className="nav-item nav-link nav-style" to="/">
           Home
         </NavLink>
-        <NavLink to="/about">
-          About
+        <NavLink className="nav-item nav-link nav-style" to="/about">
+          ABOUT
+        </NavLink>
+        <NavLink className="nav-item nav-link nav-style" to="/partners">
+          PARTNERS
+        </NavLink>
+        <NavLink className="nav-item nav-link nav-style" to="/causes">
+          CAUSES
+        </NavLink>
+        <NavLink className="nav-item nav-link nav-style" to="/partners">
+          GENRE
+        </NavLink>
+        <NavLink className="nav-item nav-link nav-style" to="/partners">
+          CHANNELS
         </NavLink>
       </nav>
     )
