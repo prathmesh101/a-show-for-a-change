9f05b83be0d1e6ecae9d858753a58d8a6fa42606
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-09 17:28:57 -0800

    fixing hamburger menu

--- src/components/nav-pages/navbar.js
+++ src/components/nav-pages/navbar.js
@@ -10,35 +10,41 @@ import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-      <nav className="nav-item">
-        <div className="nav-start">
-          <NavLink className="navbar-brand nav-link nav-style" to="/" style={{color:'white', fontFamily:'Ailerons, Regular', paddingRight:'20px', fontSize:'14px'}}>
-            SAGE APE
-          </NavLink>
-          <NavLink className="nav-link nav-style" to="/about">
-            ABOUT
-          </NavLink>
-          <NavLink className="nav-link nav-style" to="/partners">
-            PARTNERS
-          </NavLink>
-          <NavLink className="nav-link nav-style" to="/causes">
-            CAUSES
-          </NavLink>
-          <NavLink className="nav-link nav-style" to="/partners">
-            GENRE
-          </NavLink>
-          <NavLink className="nav-link nav-style" to="/partners">
-            CHANNELS
-          </NavLink>
-        </div>
-        
-        <div className="nav-end">
-        <NavLink className="nav-link nav-style nav-end" to="/sign-in">
-          SIGN IN
-        </NavLink>
-        <NavLink className="nav-link nav-style nav-end" to="/sign-up">
-          SIGN UP
-        </NavLink>    
+      <nav className="navbar">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="mainMenu">
+          <div className="nav-start">
+            <NavLink className="navbar-brand nav-item nav-link nav-style" to="/" style={{color:'white', fontFamily:'Ailerons, Regular', paddingRight:'20px', fontSize:'14px'}}>
+              SAGE APE
+            </NavLink>
+            <NavLink className="nav-item nav-link nav-style" to="/about">
+              ABOUT
+            </NavLink>
+            <NavLink className="nav-item nav-link nav-style" to="/partners">
+              PARTNERS
+            </NavLink>
+            <NavLink className="nav-item nav-link nav-style" to="/causes">
+              CAUSES
+            </NavLink>
+            <NavLink className="nav-item nav-link nav-style" to="/partners">
+              GENRE
+            </NavLink>
+            <NavLink className="nav-item nav-link nav-style" to="/partners">
+              CHANNELS
+            </NavLink>
+          </div>
+          
+          <div className="nav-end">
+            <NavLink className="nav-item nav-link nav-style nav-end" to="/sign-in">
+              SIGN IN
+            </NavLink>
+            <NavLink className="nav-item nav-link nav-style nav-end" to="/sign-up">
+              SIGN UP
+            </NavLink>    
+          </div>
         </div>
             
       </nav>
