0e9057975112df286943df475bbb38c87c490d86
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-11 13:50:39 -0800

    navbar responsivenss

--- src/components/nav-pages/navbar.js
+++ src/components/nav-pages/navbar.js
@@ -16,7 +16,7 @@ const NavBar = () => {
         </button>
         <div className="collapse navbar-collapse" id="mainMenu">
           <div className="nav-start navbar-nav mr-auto mt-2">
-            <NavLink className="nav-link nav-style" to="/" style={{color:'white', fontFamily:'Ailerons, Regular', paddingRight:'20px', fontSize:'18px'}}>
+            <NavLink className="nav-link nav-style" to="/" style={{color:'white', fontFamily:'Ailerons, Regular', paddingRight:'20px', fontSize:'16px'}}>
               SAGE APE
             </NavLink>
             <NavLink className="nav-link nav-style" to="/about">
