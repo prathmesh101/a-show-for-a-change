3ba4cb1dfdfa8c91a171a275f1f12fcf1f90a99c
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-14 09:50:44 -0800

    changes to home page

--- src/components/nav-pages/home.js
+++ src/components/nav-pages/home.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import '../../assets/css/home.css';
 
 import BackgroundImg from './background-video';
 import Genre from './genre';
 import Features from './features';
 import Join from './join-now';
-import SignUp from './sign-up';
-import Footer from '../footer';
+//import SignUp from './sign-up';
+//import Footer from '../footer';
 
 const Home = () => {
 	return (
@@ -15,10 +15,7 @@ const Home = () => {
 			<BackgroundImg />
 			<Features />
 			<Genre />
-
 			<Join />
-
-			<Footer />
 		</div>
 	)
 }
