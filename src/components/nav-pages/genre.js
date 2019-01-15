3f28f42682ebcf7a10cfb84e28741efaa4da9879
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-14 16:29:03 -0800

    fixing genre margin bottom

--- src/components/nav-pages/genre.js
+++ src/components/nav-pages/genre.js
@@ -24,7 +24,7 @@ const Genre = () => {
                <source src="http://dl1cz9lqgee4z.cloudfront.net/Cumplaneara.mp4" type="video/mp4" />
               </video>
               <h3 className="mb-3 genre-cont-head">Cumpleañera</h3>
-              <p className=" mb-3 genre-cont-para">On her 13th birthday, Gabriela Morales is given a <br />magical heirloom that could change her future.</p>
+              <p className=" mb-5 genre-cont-para">On her 13th birthday, Gabriela Morales is given a <br />magical heirloom that could change her future.</p>
             </div>
           </div>
           <div className="col-lg-4 col-md-6 text-center">
@@ -33,7 +33,7 @@ const Genre = () => {
                <source src="http://dl1cz9lqgee4z.cloudfront.net/Cumplaneara.mp4" type="video/mp4" />
               </video>
               <h3 className="mb-3 genre-cont-head">Physioshark</h3>
-              <p className=" mb-3 genre-cont-para">Short documentary about how climate change <br />threatens shark populations worldwide. </p>
+              <p className=" mb-5 genre-cont-para">Short documentary about how climate change <br />threatens shark populations worldwide. </p>
             </div>
           </div>
           <div className="col-lg-4 col-md-6 text-center">
@@ -42,7 +42,7 @@ const Genre = () => {
                <source src="http://dl1cz9lqgee4z.cloudfront.net/Cumplaneara.mp4" type="video/mp4" />
               </video>
               <h3 className="mb-3 genre-cont-head">Kharam</h3>
-              <p className=" mb-3 genre-cont-para">An eye-opening feature film about an Indian<br /> woman’s resistance to violent oppression.</p>
+              <p className=" mb-5 genre-cont-para">An eye-opening feature film about an Indian<br /> woman’s resistance to violent oppression.</p>
             </div>
           </div>
         </div>
