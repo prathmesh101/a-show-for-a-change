f0ffb32d90c698769af5241885e9d881f56023cf
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-14 16:05:43 -0800

    changes to influencer page

--- src/components/nav-pages/influencer.js
+++ src/components/nav-pages/influencer.js
@@ -34,7 +34,7 @@ const Influencer = () => {
 				</div>
 				<div className="influencer-description" style={{fontSize: 'small'}}>
 					<p>Iranian-American actress Kiana Madani has been acting, modeling
-						for nearly ten years.<br />
+						for nearly ten years.<br/>
 						Born and raised in San Jose, California, Kiana
 						partook in <span style={{textDecoration: 'underline'}}>Read more</span>  
 					</p>
