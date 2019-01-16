126340267423b3fb740e4d4fe54b0308bca0762e
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-15 16:39:19 -0800

    adding payment to influencer page

--- src/components/nav-pages/influencer.js
+++ src/components/nav-pages/influencer.js
@@ -26,6 +26,20 @@ const Influencer = () => {
 						</div>
 						<div className="modal-body">
 							<p>Here goes stripe payment</p>
+							<form action="/charge" method="post" id="payment-form">
+  								<div className="form-row">
+							    <label for="card-element">
+							      Credit or debit card
+							    </label>
+							    <div id="card-element">
+							     {/*  A Stripe Element will be inserted here.   */}
+							    </div>
+							    	{/*  Used to display form errors.   */}
+							    <div id="card-errors" role="alert"></div>
+							  </div>
+
+							  <button>Submit Payment</button>
+							</form>
 						</div>
 					</div>{/* modal-content */}
 				</div>{/* modal-dialog */}
