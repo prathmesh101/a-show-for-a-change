a88b41e581cc6603e85e1f420d6cc4e0e23036fa
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-22 17:57:47 -0800

    payment logic

--- src/components/nav-pages/influencer.js
+++ src/components/nav-pages/influencer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap';
 import kiana_hero from '../../assets/img/influencer/Kiana_HeroImage@2x.png';
 import kiana_pitch from '../../assets/img/influencer/Kiana_Screenshot.png';
 import kiana_Video1 from '../../assets/img/influencer/Kiana_Video1@2x.png';
