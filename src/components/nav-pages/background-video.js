c711f6eb758065810e6a74f605db9c97b5d187fe
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-24 23:05:33 -0800

    changing landing page video

--- src/components/nav-pages/background-video.js
+++ src/components/nav-pages/background-video.js
@@ -10,7 +10,7 @@ const BackVideo = () => {
         	<video loop muted autoPlay
                             className="backgrnd-video"
                             style={{objectFit: 'cover', width:'100%', height:'475px'}}
-                            src="https://www.youtube.com/watch?v=yNpff6jcSqI"
+                            src="https://d196vb1g1ujhpl.cloudfront.net/AShowForAChangeCompanyReel.mp4"
                             type="video/mp4"/>
 
              <div className="overlay">
--- src/components/nav-pages/genre.js
+++ src/components/nav-pages/genre.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import 'bootstrap';
+//import 'bootstrap';
 import '../../assets/css/genre.css';
 import { Link } from 'react-router-dom';
 
-import genreImg from '../../assets/img/cumpleanera-thumbnail.png';
+import cumpImg from '../../assets/img/cumpleanera-thumbnail.png';
+import marriedImg from '../../assets/img/married.png';
+import kharmaImg from '../../assets/img/office.png';
+
 
 const Genre = () => {
     return (
@@ -20,7 +23,7 @@ const Genre = () => {
         <div className="row">
           <div className="col-lg-4 col-md-6 text-center">
             <div className="service-box mt-5 mx-auto">
-              <video width="80%" height="400px" controls preload="metadata" id="vid" poster={genreImg}>
+              <video width="80%" height="400px" controls preload="metadata" id="vid" poster={cumpImg}>
                <source src="http://dl1cz9lqgee4z.cloudfront.net/Cumplaneara.mp4" type="video/mp4" />
               </video>
               <h3 className="mb-3 genre-cont-head">Cumpleañera</h3>
@@ -29,17 +32,17 @@ const Genre = () => {
           </div>
           <div className="col-lg-4 col-md-6 text-center">
             <div className="service-box mt-5 mx-auto">
-              <video width="80%" height="400px" controls preload="metadata" id="vid" poster={genreImg}>
-               <source src="http://dl1cz9lqgee4z.cloudfront.net/Cumplaneara.mp4" type="video/mp4" />
+              <video width="80%" height="400px" controls preload="metadata" id="vid" poster={marriedImg}>
+               <source src="http://djn1t69fp4pr0.cloudfront.net/weddingtrailer.mp4" type="video/mp4" />
               </video>
-              <h3 className="mb-3 genre-cont-head">Physioshark</h3>
-              <p className=" mb-5 genre-cont-para">Short documentary about how climate change <br />threatens shark populations worldwide. </p>
+              <h3 className="mb-3 genre-cont-head">Marriage</h3>
+              <p className=" mb-5 genre-cont-para">Marriage and all its complications. </p>
             </div>
           </div>
           <div className="col-lg-4 col-md-6 text-center">
             <div className="service-box mt-5 mx-auto">
-              <video width="80%" height="400px" controls preload="metadata" id="vid" poster={genreImg}>
-               <source src="http://dl1cz9lqgee4z.cloudfront.net/Cumplaneara.mp4" type="video/mp4" />
+              <video width="80%" height="400px" controls preload="metadata" id="vid" poster={kharmaImg}>
+               <source src="https://d2gm4ehypw4n5s.cloudfront.net/hurry.mp4" type="video/mp4" />
               </video>
               <h3 className="mb-3 genre-cont-head">Kharam</h3>
               <p className=" mb-5 genre-cont-para">An eye-opening feature film about an Indian<br /> woman’s resistance to violent oppression.</p>
