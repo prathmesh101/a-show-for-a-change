403bd5213930e6f6b4c106a8b7ff17630a05ad0b
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-30 14:24:51 -0800

    changed app.js to use class declaration instead of arrow function

--- src/components/app.js
+++ src/components/app.js
@@ -42,27 +42,29 @@ import VideoPage from './nav-pages/video-page.js';
 
 class App extends React.Component {
     render () {
-        <div>
-            <div className="spacing-div">
-                <BrowserRouter>
-                    <main>
-                        <Header />
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route path="/about" component={About} />
-                            <Route path = "/partners" component = {Partners}/>
-                            <Route path="/genre" component={Genres} />
-                            <Route path="/influencer" component={Influencer} />
-                            <Route path="/sign-in" component={SignIn} />
-                            <Route path="/sign-up" component={SignUp} />
-                            <Route path="/user-page" component={UserPage} />
-                            <Route path="/video-page" component={VideoPage} />
-                        </Switch>
-                        <Footer />
-                    </main>
-                </BrowserRouter>
+        return (
+            <div>
+                <div className="spacing-div">
+                    <BrowserRouter>
+                        <main>
+                            <Header />
+                            <Switch>
+                                <Route exact path="/" component={Home} />
+                                <Route path="/about" component={About} />
+                                <Route path = "/partners" component = {Partners}/>
+                                <Route path="/genre" component={Genres} />
+                                <Route path="/influencer" component={Influencer} />
+                                <Route path="/sign-in" component={SignIn} />
+                                <Route path="/sign-up" component={SignUp} />
+                                <Route path="/user-page" component={UserPage} />
+                                <Route path="/video-page" component={VideoPage} />
+                            </Switch>
+                            <Footer />
+                        </main>
+                    </BrowserRouter>
+                </div>
             </div>
-        </div>
+            );
     }
 };
 
