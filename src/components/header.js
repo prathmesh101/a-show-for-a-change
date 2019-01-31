2a98d0e1f8f1154a4203fd31ab8a5a153738800b
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-31 15:27:13 -0800

    passing values to user homepage

--- src/components/header.js
+++ src/components/header.js
@@ -23,7 +23,7 @@ class Header extends React.Component
 	render() {
 		return (
 			<div className="header-cont">
-            	<Navbar isLoggedIn={true}/>
+            	<Navbar isLoggedIn={false}/>
         	</div>
 		);
 	}
