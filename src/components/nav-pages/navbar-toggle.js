63de47822ecb03bdeeb44e7878dd8ed7e2052b1d
Sivak Khodaverdian <sevak7@yahoo.com>
2019-01-31 14:26:41 -0800

    passing values to user homepage

--- src/components/nav-pages/navbar-toggle.js
+++ src/components/nav-pages/navbar-toggle.js
@@ -11,16 +11,45 @@ function NavbarGuest(state) {
   return <NavbarGuestPart />;
 }
 
-const NavBarToggle = state => {
+// const NavBarToggle = state => {
 
 
-  //const isLoggedIn = state.isLoggedIn;
-  const isLoggedIn = this.state.isLoggedIn;
+//   //const isLoggedIn = state.isLoggedIn;
+//   const isLoggedIn = this.props.isLoggedIn;
 
-  if (isLoggedIn) {
-    return <NavbarUser />;
-  }
-  return <NavbarGuest />;
+//   if (isLoggedIn) {
+//     return <NavbarUser />;
+//   }
+//   return <NavbarGuest />;
+// }
+
+class NavBarToggle extends React.Component {
+
+	componentDidMount () {
+		const  { isLoggedIn }  = this.props.isLoggedIn;
+
+		if (isLoggedIn) {
+			const prog = "NavbarUser";
+		} else {
+			const prog = "NavbarGuest";
+		}
+	}
+
+
+
+	render () {
+		return (
+
+		 //const isLoggedIn = state.isLoggedIn;
+		 
+		 <prog />
+		  // if (isLoggedIn) {
+		  //   return <NavbarUser />;
+		  // }
+		  // return <NavbarGuest />;
+
+		);
+	}
 }
 
 
