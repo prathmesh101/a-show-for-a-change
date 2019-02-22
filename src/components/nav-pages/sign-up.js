9859fe8367e754c627ae846c01968f7b01569de5
Paolo Razon <plgrazon@gmail.com>
2019-02-22 12:51:55 -0800

    app - Initializing auth token generation for the app.

--- src/components/nav-pages/sign-up.js
+++ src/components/nav-pages/sign-up.js
@@ -8,8 +8,8 @@ import { Redirect } from 'react-router';
 
 class SignUp extends Component {
   constructor(props) {
-     super(props);
-     this.state = {
+    super(props);
+    this.state = {
       first_name: '',
       last_name: '',
       email: '',
@@ -20,26 +20,41 @@ class SignUp extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-   handleChange(event) {
-     this.setState({ [event.target.name]: event.target.value })
-   }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
-   handleClick(event) {
-     event.preventDefault();
-     var apiBaseUrl = "https://dev.sageape.com/api_php/signupapi.php";
-     //console.log("values", this.state.first_name, this.state.last_name, this.state.email, this.state.password);
-     //To be done:check name empty values before hitting submit
-     var self = this;
-     var params = {
-       "first_name": this.state.first_name,
-       "last_name": this.state.last_name,
-       "email": this.state.email,
-       "password": this.state.password,
+  handleClick(event) {
+    event.preventDefault();
+    var apiBaseUrl = "https://dev.sageape.com/api_php/signupapi.php";
+    //console.log("values", this.state.first_name, this.state.last_name, this.state.email, this.state.password);
+    //To be done:check name empty values before hitting submit
+    var self = this;
+    var params = {
+      "first_name": this.state.first_name,
+      "last_name": this.state.last_name,
+      "email": this.state.email,
+      "password": this.state.password,
     };
-    //var payload = this.state.first_name;
-    // axios.post(apiBaseUrl, {first_name: this.state.first_name, last_name: this.state.last_name, email: this.state.email, password: this.state.password})
-    //     .then(() => this.setState({ redirect: true }));
 
+    // axios to an endpoint for the json webtoke
+    axios({
+      method: 'post',
+      url: '/api/auth/tokens',
+      data: {
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        email: this.state.email,
+        password: this.state.password
+      }
+    })
+
+    /*****
+    Assume axios (backend) works focus on authorization
+    var payload = this.state.first_name;
+    axios.post(apiBaseUrl, {first_name: this.state.first_name, last_name: this.state.last_name, email: this.state.email, password: this.state.password})
+        .then(() => this.setState({ redirect: true }));
+    
     axios.post(apiBaseUrl, {first_name: this.state.first_name,
                             last_name: this.state.last_name,
                             email: this.state.email,
@@ -51,34 +66,31 @@ class SignUp extends Component {
           alert("Unable to register.");
         }
       });
-
-
-
-
-/*
+    */
+    /*****
       .then(function (response) {
-        //this.props.history.push('/UserPage');
-        // alert("here");
-        // alert(JSON.stringify(response));
-        // console.log(response);
-        // console.log(response);
-        //var res = JSON.parse(response);
+        this.props.history.push('/UserPage');
+        alert("here");
+        alert(JSON.stringify(response));
+        console.log(response);
+        console.log(response);
+        var res = JSON.parse(response);
         if (response.status == 200) {
-      //  if (response.status == 200) {
-          //if (response == "success") {
+          if (response.status == 200) {
+            if (response == "success") {
           console.log("registration successfull");
           alert("status is 200");
-      //    console.log("registration successfull");
-        //  alert("status is 200");
-          //var loginscreen = [];
-          // loginscreen.push(<Login parentContext={this} />);
-          //var loginmessage = "Not Registered yet.Go to registration";
-           // self.props.parentContext.setState({
-           //   loginscreen: loginscreen,
-           //   loginmessage: loginmessage,
-          //   buttonLabel: "Register",
-          //   isLogin: true
-          // });
+          console.log("registration successfull");
+          alert("status is 200");
+          var loginscreen = [];
+          loginscreen.push(<Login parentContext={this} />);
+          var loginmessage = "Not Registered yet.Go to registration";
+          self.props.parentContext.setState({
+            loginscreen: loginscreen,
+            loginmessage: loginmessage,
+            buttonLabel: "Register",
+            isLogin: true
+          });
         } else {
           alert("not equal to 200 but what it is we don't know");
         }
@@ -87,15 +99,15 @@ class SignUp extends Component {
         console.log(error);
         alert("Error is " + error.response + "  " + error.response.status + "  " + JSON.stringify(error.response.headers));
       });
-      //  } else {
-        //  alert("not equal to 200 but what it is we don't know");
-      //  }
-    //  })
-  //    .catch(function (error) {
-    //    console.log(error);
-      //  alert("Error is " + error.response + "  " + error.response.status + "  " + JSON.stringify(error.response.headers));
-    //  });
-*/
+      } else {
+        alert("not equal to 200 but what it is we don't know");
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert("Error is " + error.response + "  " + error.response.status + "  " + JSON.stringify(error.response.headers));
+    });
+    ******/
   }
 
 
@@ -104,7 +116,7 @@ class SignUp extends Component {
     const { redirect } = this.state;
 
     if (redirect) {
-      return <Redirect to='./user-page'/>;
+      return <Redirect to='./user-page' />;
     }
 
     return (
@@ -144,9 +156,9 @@ class SignUp extends Component {
           <div className="div-signUpFooter">
             <span>
               <p>Already have an account?</p>
-                <div className="div-center">
-                  <p><Link to="/sign-in" className="linkPrimary">SIGN IN</Link></p>
-                </div>
+              <div className="div-center">
+                <p><Link to="/sign-in" className="linkPrimary">SIGN IN</Link></p>
+              </div>
             </span>
           </div>
         </form>
